1d4f4b77e7271043b2259c36b5471f5ca8d23e43
Adrian <adrianporterocastillo@gmail.com>
2023-05-26 19:57:41 +0200

    Already have a camera but is not 3d persona view

--- Assets/Game/Scripts/PlayerController.cs
+++ Assets/Game/Scripts/PlayerController.cs
@@ -7,12 +7,20 @@ namespace TTProject.PlayerControl
 {
     public class PlayerController : MonoBehaviour
     {
+        [SerializeField] private float AnimBlendSpeed = 8.9f;
+        [SerializeField] private Transform cameraRoot;
+        [SerializeField] private Transform mainCamera;
+        [SerializeField] private float UpperLimit = -40f;
+        [SerializeField] private float BottomLimit = 70f;
+        [SerializeField] private float MouseSensitivity = 21.9f;
+
         private Rigidbody playerRigidbody;
         private InputManager inputManager;
         private Animator animator;
         private bool hasAnimator;
         private int _xVelHash;
         private int _yVelHash;
+        private float xRotation;
 
         private const float walkSpeed = 2f;
         private const float runSpeed = 6f;
@@ -33,6 +41,11 @@ namespace TTProject.PlayerControl
             Move();
         }
 
+        private void LateUpdate()
+        {
+            CamMovements();
+        }
+
         private void Move()
         {
             if (!hasAnimator) return;
@@ -40,8 +53,8 @@ namespace TTProject.PlayerControl
             float targetSpeed = inputManager.Run ? runSpeed : walkSpeed;
             if(inputManager.Move == Vector2.zero) targetSpeed = 0.1f;
 
-            currentVelocity.x = targetSpeed * inputManager.Move.x;
-            currentVelocity.y = targetSpeed * inputManager.Move.y;
+            currentVelocity.x = Mathf.Lerp(currentVelocity.x, inputManager.Move.x * targetSpeed, AnimBlendSpeed * Time.deltaTime);
+            currentVelocity.y = Mathf.Lerp(currentVelocity.y, inputManager.Move.y * targetSpeed, AnimBlendSpeed * Time.deltaTime);
 
             var xVelDifference = currentVelocity.x - playerRigidbody.velocity.x;
             var zVelDifference = currentVelocity.y - playerRigidbody.velocity.z;
@@ -50,6 +63,21 @@ namespace TTProject.PlayerControl
 
             animator.SetFloat(_xVelHash, currentVelocity.x);
             animator.SetFloat(_yVelHash, currentVelocity.y);
+        }
+
+        private void CamMovements()
+        {
+            if (!hasAnimator) return;
+
+            var Mouse_X = inputManager.Look.x;
+            var Mouse_Y = inputManager.Look.y;
+            mainCamera.position = cameraRoot.position;
+
+            xRotation -= Mouse_Y * MouseSensitivity * Time.deltaTime;
+            xRotation = Mathf.Clamp(xRotation, UpperLimit, BottomLimit);
+
+            mainCamera.localRotation = Quaternion.Euler(xRotation, 0, 0);
+            transform.Rotate(Vector3.up, Mouse_X * MouseSensitivity * Time.deltaTime);
         }
     }
 }
